0108c14a375e3ee66ed8f0eeee8525fc9d9674bd
leedc0101 <leedc0101@naver.com>
2021-08-16 16:58:59 +0900

    init

---
+++ src/components/IncButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from 'ethers'
+import { COUNTER_ABI} from "../constant";
+
+function IncButton() {
+    const { account, library } = useWeb3React()
+    const signer = library?.getSigner(account).connectUnchecked()
+
+    const onClick = () => {
+        const counterContract = new ethers.Contract('0x9acDeC484dFD452ce56bb666A432916702fB2F0E', COUNTER_ABI, signer)
+
+        counterContract.inc()
+            .then(result => console.log(result))
+    }
+
+    return (
+        <div>
+            <button onClick={onClick}>
+                Increase
+            </button>
+        </div>
+    )
+}
+
+export default IncButton
---
+++ src/constant/index.js
@@ -0,0 +1,4 @@
+export const COUNTER_ABI = [
+    'function get() public view returns (uint)',
+    'function inc() public'
+]
